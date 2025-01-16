e2512a206aab1aa2876c2fd736a11586b7ed6c69
Sameer <sameerrahangdale63@gmail.com>
2025-01-16 13:50:37 +0530

    Dark-Mode Project

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+let body = document.querySelector("body");
+let toggle = document.querySelector(".toggle-btn");
+let circle = document.querySelector(".circle");
 
+console.log(body,toggle,circle);
+toggle.addEventListener("click" , () => body.classList.toggle("dark-mode"));
+toggle.addEventListener("click" , () => circle.classList.toggle("active"));
